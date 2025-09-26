62744efc2655a91977dc433214f779cd2309eb71
Abhranil Das <83596540+Abhranil2021@users.noreply.github.com>
2025-09-27 00:32:53 +0530

    Added Vercel Analytics (#7)
    * Minor bug fixes
    
    * Minor bug fixes part 2
    
    * Minor fixes
    
    * Optimized Code for Production Deployment
    
    * Fixed Minor Bugs For Production
    
    * Added Vercel Speed Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import Portfolio from './components/Portfolio';
 import ErrorBoundary from './components/ErrorBoundary';
 import './styles/index.css';
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 function App() {
   return (
     <ErrorBoundary>
       <div className="App">
         <Portfolio />
+        {/* Add Vercel Speed Insights here */}
+        <SpeedInsights />
       </div>
     </ErrorBoundary>
   );
